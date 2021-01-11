e6a2d9cb0e27ef46746ae21daac3db1063527f97
Dennis Muth <d.muth@gmx.net>
2021-01-11 16:32:37 +0100

    Bump version: 0.1.0 → 0.1.1

--- pytoolkit/__init__.py
+++ pytoolkit/__init__.py
@@ -2,7 +2,7 @@
 
 from .basics import classproperty
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Dennis Muth <d.muth@gmx.net>"
 
 
--- tasks/config.py
+++ tasks/config.py
@@ -3,7 +3,7 @@ import os
 from invoke import task
 
 # VERSION
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 
 # PATH STUFF
